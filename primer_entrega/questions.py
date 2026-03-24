c72799ecb486700d3b5c8c80752e2027c3db56d1
Daiana <daianavazquez1@gmail.com>
2026-03-24 17:19:51 -0300

    primer modificacion, bug arreglado

--- primer_entrega/questions.py
+++ primer_entrega/questions.py
@@ -1,5 +1,5 @@
 import random
-
+import string
 words = [
     "python",
     "programa",
@@ -16,7 +16,7 @@ attempts = 6
 print("¡Bienvenido al Ahorcado!")
 print()
 while attempts > 0:
-# Mostrar progreso: letras adivinadas y guiones para las que faltan
+    # Mostrar progreso: letras adivinadas y guiones para las que faltan
     progress = ""
     for letter in word:
         if letter in guessed:
@@ -24,25 +24,30 @@ while attempts > 0:
         else:
             progress += "_ "
     print(progress)
-# Verificar si el jugador ya adivinó la palabra completa
+    
+    # Verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
         print("¡Ganaste!")
-    break
+        break
 
     print(f"Intentos restantes: {attempts}")
 
     print(f"Letras usadas: {', '.join(guessed)}")
 
     letter = input("Ingresá una letra: ")
-    if letter in guessed:
-        print("Ya usaste esa letra.")
-    elif letter in word:
-        guessed.append(letter)
-        print("¡Bien! Esa letra está en la palabra.")
+
+    if letter.lower() in string.ascii_lowercase:
+        if letter in guessed:
+            print("Ya usaste esa letra.")
+        elif letter in word:
+            guessed.append(letter)
+            print("¡Bien! Esa letra está en la palabra.")
+        else:
+            guessed.append(letter)
+            attempts -= 1
+            print("Esa letra no está en la palabra.")
+            print()
     else:
-        guessed.append(letter)
-        attempts -= 1
-        print("Esa letra no está en la palabra.")
-        print()
+        print("Entrada no válida")
 else:
     print(f"¡Perdiste! La palabra era: {word}")
